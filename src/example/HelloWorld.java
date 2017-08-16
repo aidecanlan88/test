4bb096c360d634482df357fc8dbde835ed15ff44
aidecanlan88 <31053578+aidecanlan88@users.noreply.github.com>
2017-08-16 13:18:34 +0800

    x

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -10,7 +10,7 @@ import javax.xml.ws.Endpoint;
 public class HelloWorld {
   @WebMethod
   public String sayHelloWorldFrom(String from) {
-    String result = "Hello, world, 终于成功了" + from;
+    String result = "Hello, world, 终于成功了，测试pr" + from;
     System.out.println(result);
     return result;
   }
